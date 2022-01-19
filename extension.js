701fd7b492a5c9bfde1ebda4b0a5536268f5d747
MIMJA156 <mimja156@gmail.com>
2022-01-18 22:18:54 -0700

    added part of the error handler

--- extension.js
+++ extension.js
@@ -1,19 +1,35 @@
 const vscode = require('vscode');
+const fs = require('fs');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
+const global = {};
+
 function activate(context) {
 	const myCommandId = 'mimjas-time-tracker.timeStatuesItemClicked';
 	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, async () => {}));
 
-	// create a new status bar item and align it left.
+	// create a new status bar item and align it to the left.
 	const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
 	item.command = myCommandId;
 	context.subscriptions.push(item);
 
-	item.text = `$(circuit-board)}`;
+	try {
+		global.full = fs.readFileSync(`${__dirname}/../time-tracker-storage-mimja/time.json`);
+	} catch (e) {
+		fs.mkdirSync(`${__dirname}/../time-tracker-storage-mimja/`);
+		fs.writeFileSync(`${__dirname}/../time-tracker-storage-mimja/time.json`, '{}');
+	}
+
+	try {
+		global.full[2022].months;
+	} catch (e) {
+
+	}
+
+	item.text = `$(circuit-board) ${timeString(1, 0)}`;
 	item.tooltip = `Time Spent Coding on ${getNumberDate()}`;
 	item.show();
 }
@@ -37,4 +53,20 @@ function getCurrentTime() {
 	const mm = today.getMonth() + 1;
 	const dd = today.getDate();
 	return [yyyy, mm, dd];
+}
+
+function timeString(hours, minutes) {
+	let h_s = `${hours} hr`;
+	let m_s = `${minutes} min`;
+
+	if (hours <= 0) h_s = '';
+	if (minutes <= 0) m_s = '';
+	if (hours > 1) h_s = `${h_s}s`;
+	if (minutes > 1) m_s = `${m_s}s`;
+
+	return `${h_s} ${m_s}`;
+}
+
+function newYearOfTimeJson() {
+
 }
