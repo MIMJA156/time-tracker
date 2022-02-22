1a6365e5d757495219eac52a840a8002ac80dee2
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-02-22 01:18:11 -0700

    basically made the data formatting - 99% done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const port = 3457;
 const Express = require('express');
+const {
+    emit
+} = require('process');
 
 const global = {};
 
@@ -14,8 +17,133 @@ function bootServer() {
 
     app.listen(port);
 
-    app.post('/', async (req, res) => {
-        console.log(req);
+    app.post('/api/get-data', async (req, res) => {
+        const unParsed = fs.readFileSync(`${__dirname}/../${global.fileDir}/${global.fileName}.json`, 'utf8');
+        const parsed = JSON.parse(unParsed);
+
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1;
+        const day = today.getDate();
+
+        let graphDataChanged = {};
+
+        let firstYear = parseFloat(Object.keys(parsed)[Object.keys(parsed).length - Object.keys(parsed).length]);
+        let firstMonth = parseFloat(Object.keys(parsed[firstYear])[Object.keys(parsed[firstYear]).length - Object.keys(parsed[firstYear]).length]);
+        let firstDay = parseFloat(Object.keys(parsed[firstYear][firstMonth])[Object.keys(parsed[firstYear][firstMonth]).length - Object.keys(parsed[firstYear][firstMonth]).length]);
+
+        let lastYear = parseFloat(Object.keys(parsed)[Object.keys(parsed).length - 1]);
+        let lastMonth = parseFloat(Object.keys(parsed[lastYear])[Object.keys(parsed[lastYear]).length - 1]);
+        let lastDay = parseFloat(Object.keys(parsed[lastYear][lastMonth])[Object.keys(parsed[lastYear][lastMonth]).length - 1]);
+
+        var dayKey = {
+            'sunday': 0,
+            'monday': 1,
+            'tuesday': 2,
+            'wednesday': 3,
+            'thursday': 4,
+            'friday': 5,
+            'saturday': 6
+        };
+
+        if (parsed[firstYear][firstMonth][firstDay].day != 'sunday') {
+            let loop = await loopTillValue(dayKey[parsed[firstYear][firstMonth][firstDay].day], 0, '<');
+            firstDay -= loop;
+
+            if (firstDay < 1) {
+                firstMonth--;
+                if (firstMonth < 1) {
+                    firstYear--;
+                    firstMonth = 12;
+                }
+
+                firstDay = getDaysInMonth(firstMonth, firstYear);
+            }
+        }
+
+        if (parsed[lastYear][lastMonth][lastDay].day != 'saturday') {
+            let loop = await loopTillValue(dayKey[parsed[lastYear][lastMonth][lastDay].day], 6, '>');
+            lastDay += loop;
+
+            if (lastDay > getDaysInMonth(lastMonth, lastYear)) {
+                lastMonth++;
+                if (lastMonth > 12) {
+                    lastYear++;
+                    lastMonth = 1;
+                }
+
+                lastDay = 1;
+            }
+        }
+
+        let getActiveArray = () => {
+            let activeArray = [];
+            let z = firstYear;
+            let y = firstMonth;
+            let x = firstDay;
+
+            for (let i = 0; i < 7; i++) {
+                x++;
+
+                if (x > getDaysInMonth(firstMonth, firstYear)) {
+                    y += 1;
+                    if (y > 12) {
+                        z += 1;
+                        y = 1;
+                    }
+                    x = 1;
+                }
+
+                try {
+                    activeArray[i] = parsed[z][y][x]['active'];
+                } catch (e) {
+                    activeArray[i] = 0;
+                }
+            }
+
+            return activeArray;
+        }
+
+        loopThroughAndSetData();
+
+        function loopThroughAndSetData(depth) {
+            depth = depth ? depth : 0;
+
+            let a = firstDay + 6;
+            let b = firstMonth;
+            let c = firstYear;
+
+            if (a > getDaysInMonth(firstMonth, firstYear)) {
+                b += 1;
+
+                if (b > 12) {
+                    c += 1;
+                    b = 1;
+                }
+
+                a = 1;
+            }
+
+            graphDataChanged[`${firstYear}-${firstMonth}-${firstDay}/${c}-${b}-${a}`] = {
+                active: getActiveArray()
+            };
+
+            firstDay = a + 1;
+            firstMonth = b;
+            firstYear = c;
+
+            if (`${c}-${b}-${a}` === `${lastYear}-${lastMonth}-${lastDay}`) return;
+
+            loopThroughAndSetData(depth + 1);
+        }
+
+        console.log(loopTillValue(dayKey[parsed[firstYear][firstMonth][firstDay].day], 0, '<'));
+
+        for (let i = 0; i < 8; i++) {
+            console.log(graphDataChanged[``]);
+        }
+
+        res.send(graphDataChanged);
     });
 
     return port;
