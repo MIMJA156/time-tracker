c1664a8579979213b332658ce1d6345267026db4
MIMJA156 <mimja156@gmail.com>
2022-03-04 14:36:40 -0700

    added fun functions

--- src/server.js
+++ src/server.js
@@ -33,3 +33,52 @@ function bootServer() {
         return port;
     });
 }
+
+//Useful functions.
+
+/**
+ * @param {number} month 
+ * @param {number} year 
+ * @returns {number} The number of days in the month specified.
+ */
+function getDaysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+};
+
+/**
+ * @param {number} val The number you want to check
+ * @param {number} till The number you want to loop to
+ * @param {string} sign Use `<` or `>`
+ * @returns The number of times it took to reach `till` from `val`
+ */
+async function loopTillValue(val, till, sign) {
+    let loops = 0;
+    let depth = 7;
+
+    let internalLoop = (val, till, sign, loops, resolve, reject) => {
+        if (loops > depth) reject('Too many loops.');
+        if (sign === '>') {
+            if (val >= till) {
+                resolve(loops);
+            } else {
+                loops++;
+                val++;
+                internalLoop(val, till, sign, loops, resolve, reject);
+            }
+        } else if (sign === '<') {
+            if (val <= till) {
+                resolve(loops);
+            } else {
+                loops++;
+                val--;
+                internalLoop(val, till, sign, loops, resolve, reject);
+            }
+        }
+    }
+
+    let isDone = new Promise((resolve, reject) => {
+        internalLoop(val, till, sign, loops, resolve, reject);
+    })
+
+    return await isDone;
+}
