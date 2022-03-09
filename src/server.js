646aca8d5c23f10b9877a728d58ba0942106d9fa
MIMJA156 <mimja156@gmail.com>
2022-03-09 15:53:23 -0700

    HELP ME PLEASE

--- src/server.js
+++ src/server.js
@@ -27,8 +27,6 @@ function bootServer() {
             current: "L"
         };
 
-        let currentDayIndex = new Date().getDay();
-
         let firstRecordedYear = parseFloat(Object.keys(parsed)[Object.keys(parsed).length - Object.keys(parsed).length]);
         let firstRecordedMonth = parseFloat(Object.keys(parsed[firstRecordedYear])[Object.keys(parsed[firstRecordedYear]).length - Object.keys(parsed[firstRecordedYear]).length]);
         let firstRecordedDay = parseFloat(Object.keys(parsed[firstRecordedYear][firstRecordedMonth])[Object.keys(parsed[firstRecordedYear][firstRecordedMonth]).length - Object.keys(parsed[firstRecordedYear][firstRecordedMonth]).length]);
@@ -36,6 +34,8 @@ function bootServer() {
         let currentRecordedYear = parseFloat(Object.keys(parsed)[Object.keys(parsed).length - 1]);
         let currentRecordedMonth = parseFloat(Object.keys(parsed[currentRecordedYear])[Object.keys(parsed[currentRecordedYear]).length - 1]);
         let currentRecordedDay = parseFloat(Object.keys(parsed[currentRecordedYear][currentRecordedMonth])[Object.keys(parsed[currentRecordedYear][currentRecordedMonth]).length - 1]);
+
+        let currentDayIndex = new Date(`${currentRecordedYear}-${currentRecordedMonth}-${currentRecordedDay}`).getDay();
 
         console.log(`First year: ${firstRecordedYear}/${firstRecordedMonth}/${firstRecordedDay}`);
         console.log(`Current Date: ${currentRecordedYear}/${currentRecordedMonth}/${currentRecordedDay}`);
@@ -47,8 +47,14 @@ function bootServer() {
             tillValues[0] = getDaysInMonth(currentRecordedMonth - 1, currentRecordedYear) + tillValues[0];
         }
 
-        console.log(tillValues[0]);
-        console.log(tillValues[1]);
+        let currentWeek = [];
+
+        console.log(currentDayIndex);
+
+        let tillCurrentWeekEnd = await loopTillValue(currentDayIndex, 6, '>');
+        let tillCurrentWeekStart = await loopTillValue(currentDayIndex, 0, '<');
+
+        let lastWeek = [];
 
         res.send(graphDataChanged);
     });
