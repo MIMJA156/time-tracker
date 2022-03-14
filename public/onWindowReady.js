fde81dcc8761fbd22db2b278b56c74eea9488b47
MIMJA156 <mimja156@gmail.com>
2022-03-14 17:24:00 -0600

    added moving settings bar

--- public/onWindowReady.js
+++ public/onWindowReady.js
@@ -31,6 +31,71 @@ function windowIsReady() {
         updateChart();
         updateSettings();
     });
+
+    //Make settings draggable
+    dragElement(document.getElementById("settings-menu-body"));
+
+    let shown = false;
+
+    function dragElement(elmnt) {
+        var pos1 = 0,
+            pos2 = 0,
+            pos3 = 0,
+            pos4 = 0;
+        if (document.getElementById("settings-header")) {
+            // if present, the header is where you move the DIV from:
+            document.getElementById("settings-header").onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+            if (shown === false) {
+                $('#reset-settings-pos').css('display', 'block');
+                shown = true;
+            }
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+
+        function resetPos() {
+            elmnt.style.top = '50%';
+            elmnt.style.left = '50%';
+        }
+
+        $('#reset-settings-pos').on('click', () => {
+            resetPos();
+            $('#reset-settings-pos').css('display', 'none');
+            shown = false;
+        });
+    }
 }
 
 function updateSettings() {
